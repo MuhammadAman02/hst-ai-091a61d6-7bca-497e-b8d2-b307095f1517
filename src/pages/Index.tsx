0e1271baa99fd1355b1758f368f812e75da6e7c6
MuhammadAman02 <mohammada@hst.ie>
2025-06-05 15:14:29 +0500

    update file

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,12 @@
 import ChatBot from "@/components/ChatBot";
+import EnvCheck from "@/components/EnvCheck";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <div className="container mx-auto max-w-4xl p-4">
+        <EnvCheck />
+      </div>
       <ChatBot />
     </div>
   );
